ad2bd8e681f4411f2f2c63dadd40c53c47c1e875
Matthieu Ippersiel <matthieu.ippersiel@gmail.com>
2024-05-07 21:32:22 -0400

    Removed python autoflake as it was deprecated by none-ls

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -13,7 +13,6 @@ return {
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "ruff-lsp")
       table.insert(opts.ensure_installed, "pyright")
-      table.insert(opts.ensure_installed, "autoflake") -- Fixes imports
       table.insert(opts.ensure_installed, "black") -- Formatter
     end,
   },
@@ -21,7 +20,6 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.autoflake)
       table.insert(opts.sources, nls.builtins.formatting.black)
     end,
   },
