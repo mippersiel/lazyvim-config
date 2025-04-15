06930e935205c879703f9a9569edaafb4a3ff4cb
Matthieu Ippersiel <mippersiel@orthogone.com>
2025-04-15 16:59:51 -0400

    -Added default c/c++ clang-format when file is not available in project -Added capability to format blocks of code in <leader>F key binding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- stylua: ignore
+vim.keymap.set("v", "<leader>F", function() require("conform").format({ async = true, lsp_fallback = true }) end, { desc = "Format selected" })
--- lua/languages/c_cpp.lua
+++ lua/languages/c_cpp.lua
@@ -9,7 +9,45 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        sh = { "clang-format" },
+        c = { "my_clang_format" },
+        cpp = { "my_clang_format" },
+      },
+      formatters = {
+        my_clang_format = {
+          command = "clang-format",
+          args = function(_, _) -- (self, context)
+            -- Choose project's .clang-format file, otherwise use locally configured one
+            local clang_format_file = vim.fs.joinpath(require("lazyvim.util").root.get(), ".clang-format")
+            if not vim.uv.fs_stat(clang_format_file) then
+              clang_format_file = vim.fn.expand("~/.config/nvim/default-settings/clang-format")
+            end
+            return {
+              "--style=file:" .. clang_format_file,
+              "-assume-filename",
+              "$FILENAME",
+            }
+          end,
+          range_args = function(_, ctx) -- (self, context)
+            local util = require("conform.util")
+            local start_offset, end_offset = util.get_offsets_from_range(ctx.buf, ctx.range)
+            local length = end_offset - start_offset
+            -- Choose project's .clang-format file, otherwise use locally configured one
+            local clang_format_file = vim.fs.joinpath(require("lazyvim.util").root.get(), ".clang-format")
+            if not vim.uv.fs_stat(clang_format_file) then
+              clang_format_file = vim.fn.expand("~/.config/nvim/default-settings/clang-format")
+            end
+            return {
+              "--style=file:" .. clang_format_file,
+              "-assume-filename",
+              "$FILENAME",
+              "--offset",
+              tostring(start_offset),
+              "--length",
+              tostring(length),
+            }
+          end,
+          stdin = true,
+        },
       },
     },
   },
